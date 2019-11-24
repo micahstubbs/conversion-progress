d74298f40077555f6223d42ddff5185db50ce09f
Micah Stubbs <micah.stubbs@gmail.com>
2019-11-23 16:48:57 -0800

    add script to get the commit list as an array of objects

--- getCommitList/index.js
+++ getCommitList/index.js
@@ -2,5 +2,33 @@ import runGitCommand from '../runGitCommand/index.js'
 
 export default function() {
   const getCommitList = "git log --oneline | nl -v0 | sed 's/^ \+/&HEAD~/'"
-  return runGitCommand(getCommitList)
+  const commitListString = runGitCommand(getCommitList)
+
+  const lines = commitListString.split('\n')
+  // console.log('lines', lines)
+
+  const parsedLines = lines.map((line) => parseLine(line))
+  // console.log('parsedLines', parsedLines)
+  return parsedLines
+}
+
+function parseLine(line) {
+  let newLine = ''
+  // console.log('line\n', line)
+  
+  newLine = line.trim()
+  // console.log('newLine after first trim\n', newLine)
+
+  const index = newLine.slice(0,1) 
+  newLine = newLine.slice(1).trim()
+  // console.log('newLine\n', newLine)
+  // console.log('###### end parseLine #######\n')
+  const hash = newLine.slice(0,7)
+  newLine = newLine.slice(7).trim()
+  const message = newLine
+  return {
+    index, 
+    hash,
+    message
+  }
 }
--- getCommitList/test.js
+++ getCommitList/test.js
@@ -2,5 +2,5 @@ import getCommitList from './index.js'
 
 const commitList = getCommitList()
 
-console.log(`commit list:\n ${commitList}`)
+console.log(`commit list:\n ${JSON.stringify(commitList, null, 2)}`)
 
