57ab628181f6ed8c1e18f9eece392af278ed15c0
Micah Stubbs <micah.stubbs@gmail.com>
2019-11-23 17:06:56 -0800

    add design for getProgressOverCommits

---
+++ getProgressOverCommits/index.js
@@ -0,0 +1,15 @@
+import getConversionProgress from '../index.js'
+import getCommitList from '../getCommitList/index.js'
+
+export default function (projectDir, sourceExts, targetExts) {
+  // get the list of commits in the project
+
+  // loop over that list 
+  // get progress for each commit
+  // stop if the progress is 0 for two commits in a row
+
+  // as a side effect
+  // write the result out to a csv file
+
+  // return the result
+}
