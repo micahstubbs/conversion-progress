bbc2a1e4011fbf81e6e72d17f0bea11d0d4c419f
Micah Stubbs <micah.stubbs@gmail.com>
2019-11-23 17:59:29 -0800

    add zero progress logic

--- getProgressOverCommits/index.js
+++ getProgressOverCommits/index.js
@@ -1,7 +1,7 @@
 import getConversionProgress from '../index.js'
 import getCommitList from '../getCommitList/index.js'
 
-export default function (projectDir, sourceExts, targetExts) {
+export default function(projectDir, sourceExts, targetExts) {
   // get the list of commits in the project
   console.log(`getting commits for ${projectDir}`)
   const commitList = getCommitList(projectDir)
@@ -9,15 +9,26 @@ export default function (projectDir, sourceExts, targetExts) {
 
   let zeroProgressCounter = 0
 
-  // loop over that list 
+  // loop over that list
   // get progress for each commit
-  // stop if the progress is 0 for two commits in a row
-  
   commitList.some(commit => {
+    const progress = getConversionProgress(
+      projectDir,
+      sourceExts,
+      targetExts,
+      commit.hash
+    )
     result.push({
       ...commit,
-      ...getConversionProgress(projectDir, sourceExts, targetExts, commit.hash)
+      ...progress
     })
+
+    // update the counter
+    if (progress.percentConverted === 0) zeroProgressCounter += 1
+    else zeroProgressCounter = 0
+
+    // stop if the progress is 0 for two commits in a row
+    if (zeroProgressCounter >= 2) return true
   })
 
   // as a side effect
